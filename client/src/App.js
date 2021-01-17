a02807b8ec3be855b9b87829db1bd756f063d731
helenohyeah <ouyang.helen@gmail.com>
2021-01-17 16:00:11 -0500

    Hook up client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  axios.defaults.baseURL = 'http://localhost:8080';
+  axios.get('/')
+    .then(res => console.log('Response:', res))
+    .catch(err => console.log('Error:', err));
+
   return (
-    <h1>Hello Dispatcher</h1>
+    <>
+      <h1>Hello Dispatcher</h1>
+    </>
   );
 }
 

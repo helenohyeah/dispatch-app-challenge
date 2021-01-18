2c16f886a04a2722e281747dc2066687cc31d65e
helenohyeah <ouyang.helen@gmail.com>
2021-01-18 14:46:01 -0500

    Set up functionality to get and post a movement task

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,15 +1,16 @@
 export default function Form() {
+  const url = 'http://localhost:8080/api/tasks';
   return (
     <>
       <h2>Create New Task</h2>
-      <form>
-        <label htmlFor="start-loc">Start Location:</label>
-        <input type="text" name="start-loc"></input>
-        <label htmlFor="end-loc">End Location:</label>
-        <input type="text" name="end-loc"></input>
-        <label htmlFor="freight">Freight Description:</label>
-        <input type="text" name="freight"></input>
-        <input type="submit" value="Create"></input>
+      <form action={url} method='POST'>
+        <label htmlFor='start'>Start Location:</label>
+        <input type='text' name='start'></input>
+        <label htmlFor='end'>End Location:</label>
+        <input type='text' name='end'></input>
+        <label htmlFor='freight'>Freight Description:</label>
+        <input type='text' name='freight'></input>
+        <input type='submit' value='Create'></input>
       </form>
     </>
   );
--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Tasks() {
+
+  const [tasks, setTasks] = useState([]);
+
   axios.defaults.baseURL = 'http://localhost:8080';
   useEffect(() => {
     axios.get('/api/tasks')
-      .then(res => console.log(res))
+      .then(res => setTasks(res.data))
       .catch(err => console.log(err));
   }, []);
+  
+  const tasksList = tasks.map(task => <li key={task.id}>{task.start} to {task.end} - Freight: {task.freight}</li>);
 
   return (
     <>
       <h2>Tasks</h2>
+      <ul>
+        {tasksList}
+      </ul>
     </>
   )
 }

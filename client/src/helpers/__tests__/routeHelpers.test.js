1993dcfe77b0dc78defe6133fa768da963af3cfc
helenohyeah <ouyang.helen@gmail.com>
2021-01-20 21:49:03 -0500

    Update route helper test to new location data structure

--- client/src/helpers/__tests__/routeHelpers.test.js
+++ client/src/helpers/__tests__/routeHelpers.test.js
@@ -1,92 +1,20 @@
 import { generateNodes } from '../routeHelpers';
 
-// Mock data
-const tasks = [
-  {
-    "id": 1,
-    "color": "FF0000",
-    "startLat": 45.502,
-    "startLng": -73.567,
-    "endLat": 43.653,
-    "endLng": -79.383,
-    "freight": "Montreal->Toronto, Produce"
-  },
-  {
-    "id": 2,
-    "color": "008000",
-    "startLat": 45.422,
-    "startLng": -75.697,
-    "endLat": 43.653,
-    "endLng": -79.383,
-    "freight": "Ottawa->Toronto, Groceries"
-  },
-  {
-    "id": 3,
-    "color": "0000FF",
-    "startLat": 43.813,
-    "startLng": -79.495,
-    "endLat": 45.502,
-    "endLng": -73.567,
-    "freight": "Concord->Montreal, Produce"
-  }
-]
-
-const nodes = {
-  start: [
-    {
-      tasks: [1],
-      location: { lat: 45.502, lng: -73.567 },
-      visited: false
-    },
-    {
-      tasks: [2],
-      location: { lat: 45.422, lng: -75.697 },
-      visited: false
-    },
-    {
-      tasks: [3],
-      location: { lat: 43.813, lng: -79.495 },
-      visited: false
-    },
-  ],
-  end: [
-    {
-      tasks: [1],
-      location: { lat: 43.653, lng: -79.383 },
-      visited: false
-    },
-    {
-      tasks: [2],
-      location: { lat: 43.653, lng: -79.383 },
-      visited: false
-    },
-    {
-      tasks: [1],
-      location: { lat: 45.502, lng: -73.567 },
-      visited: false
-    }
-  ]
-};
-
 describe('generateNodes helper', () =>{
   it('should return a list of start and end nodes given list of unique locations', () => {
     const tasks = [
       {
         "id": 1,
         "color": "FF0000",
-        "startLat": 45.502,
-        "startLng": -73.567,
-        "endLat": 43.653,
-        "endLng": -79.383,
+        "start": { lat: 45.502, lng: -73.567 },
+        "end": { lat: 43.653, lng: -79.383 },
         "freight": "Montreal->Toronto, Produce"
       },
       {
         "id": 2,
         "color": "008000",
-        "startLat": 45.422,
-        "startLng": -75.697,
-        "endLat": 43.732,
-        "endLng": -79.762,
+        "start": { lat: 45.422, lng: -75.697 },
+        "end": { lat: 43.732, lng: -79.762 },
         "freight": "Ottawa->Brampton, Groceries"
       }
     ];
@@ -125,19 +53,15 @@ describe('generateNodes helper', () =>{
       {
         "id": 1,
         "color": "FF0000",
-        "startLat": 45.502,
-        "startLng": -73.567,
-        "endLat": 43.653,
-        "endLng": -79.383,
+        "start": { lat: 45.502, lng: -73.567 },
+        "end": { lat: 43.653, lng: -79.383 },
         "freight": "Montreal->Toronto, Produce"
       },
       {
         "id": 2,
         "color": "008000",
-        "startLat": 45.422,
-        "startLng": -75.697,
-        "endLat": 43.653,
-        "endLng": -79.383,
+        "start": { lat: 45.422, lng: -75.697 },
+        "end": { lat: 43.653, lng: -79.383 },
         "freight": "Ottawa->Toronto, Groceries"
       }
     ];

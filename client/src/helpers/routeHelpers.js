c3b78ec6704056d5e19ae41ceae1efb9f6e437a7
helenohyeah <ouyang.helen@gmail.com>
2021-01-20 21:13:58 -0500

    Create notes for route finding algorithm and set up initial helper functions

---
+++ client/src/helpers/routeHelpers.js
@@ -0,0 +1,29 @@
+// Algorithim
+// Loop through each start node
+// Keep track of total distance
+// On each start node pick the closest next node from available nodes
+  // Available nodes = all unvisited start nodes and end node of tasks 'assigned'
+  // Set that start node to visited
+  // Add distance to total distance
+  // Repeat until all nodes are visited
+// Pick the route with the shortest overall distance
+
+// Return a list of start and end nodes given tasks
+function generateNodes(tasks) {
+  return false;
+}
+
+// Return the distance between two nodes
+function calcNodeDistance(a, b) {
+
+}
+
+// Returns the distance of a route given a start node and all nodes
+function calcRouteDistance() {
+
+}
+
+
+
+
+export { generateNodes };
